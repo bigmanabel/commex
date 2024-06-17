9e79809ad90371333782a3930c24dd100fd2119a
Abel Nutsugah <bigmanabel@outlook.com>
2024-06-17 13:39:56 +0000

    test deployment

--- src/app/home/main/filters/price-filter/price-filter.component.ts
+++ src/app/home/main/filters/price-filter/price-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-price-filter',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './price-filter.component.css'
 })
 export class PriceFilterComponent {
+  min!: number;
+  max!: number;
+  @Output() onPriceRangeSet = new EventEmitter<{min: number, max: number}>();
 
+  setPriceRange() {
+    if (this.min && this.max && this.min < this.max) {
+      console.log('Price range set');
+      this.onPriceRangeSet.emit({min: this.min, max: this.max});
+    } else
+    console.log('Error')
+  }
 }
